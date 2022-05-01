22cddf587a1714d1c9ef5baccf4162b3ed8e61a0
Antonio Carlos Guimarães de Faria Junior <antonio.junior@stz.com.br>
2022-05-01 14:53:14 -0300

    criacao do projeto

---
+++ src/main/java/com/dbc/analyzerDat/AnalyzerDatApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.analyzerDat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerDatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerDatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbc/analyzerDat/AnalyzerDatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.analyzerDat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerDatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
